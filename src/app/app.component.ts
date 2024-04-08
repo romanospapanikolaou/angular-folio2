19e44e14b10e9cd3eaa81c5ae4bd4aef1495d4b8
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-04-09 00:21:09 +0200

    more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeaderComponent, HeroComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    FooterComponent,
+  ],
 })
 export class AppComponent {
   title = 'angular-folio2';
