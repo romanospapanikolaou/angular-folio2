1b81093eb0f693c236ca6b9097a21b5b1bd2e0b1
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-04-24 16:11:59 +0200

    click to copy

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -5,8 +5,25 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
-export class ContactComponent {
+export class ContactComponent {}
 
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const emailButton: HTMLElement | null = document.querySelector('.email');
+  const toCopyDiv: HTMLElement | null = document.querySelector('.to-copy');
+
+  if (emailButton && toCopyDiv) {
+    emailButton.addEventListener('click', () => {
+      const email: string = emailButton.textContent || '';
+      navigator.clipboard.writeText(email).then(
+        () => {
+          console.log('Email copied to clipboard');
+        },
+        (err) => {
+          console.error('Error copying email to clipboard: ', err);
+        }
+      );
+    });
+  }
+});
