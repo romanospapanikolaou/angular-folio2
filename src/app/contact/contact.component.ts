e7e33bdc76ea5c4c588c78647861689e8e4de110
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-04-24 16:29:03 +0200

    contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,13 +12,20 @@ export class ContactComponent {}
 document.addEventListener('DOMContentLoaded', () => {
   const emailButton: HTMLElement | null = document.querySelector('.email');
   const toCopyDiv: HTMLElement | null = document.querySelector('.to-copy');
+  const copiedMessage: HTMLElement | null =
+    document.querySelector('.copied-message');
 
-  if (emailButton && toCopyDiv) {
+  if (emailButton && toCopyDiv && copiedMessage) {
     emailButton.addEventListener('click', () => {
       const email: string = emailButton.textContent || '';
       navigator.clipboard.writeText(email).then(
         () => {
           console.log('Email copied to clipboard');
+          toCopyDiv.style.display = 'none';
+          copiedMessage.style.display = 'block';
+          setTimeout(() => {
+            copiedMessage.style.display = 'none';
+          }, 2000); // Hide after 5 seconds
         },
         (err) => {
           console.error('Error copying email to clipboard: ', err);
