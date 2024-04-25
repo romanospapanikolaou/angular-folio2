2c6043ff85c66746a52ab7ba7dd70ddd37173723
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-04-25 21:22:46 +0200

    animation

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -12,24 +12,44 @@ export class HeroComponent {}
 
 window.addEventListener('scroll', () => {
   const scrollPosition: number = window.scrollY;
-  const heroTitle: HTMLElement | null = document.querySelector(
+  const heroTitleLeft: HTMLElement | null = document.querySelector(
     '.hero__title__left'
   ) as HTMLElement;
 
-  if (heroTitle) {
-    const moveDistance: number = 1.5; // Adjust this value to change the distance
-    heroTitle.style.marginLeft = `-${scrollPosition * moveDistance}px`;
+  if (heroTitleLeft) {
+    if (window.innerWidth > 765) {
+      const moveDistance: number = 0.7; // Adjust this value to change the distance
+      heroTitleLeft.style.marginLeft = `-${scrollPosition * moveDistance}px`;
+    } else {
+      heroTitleLeft.style.marginLeft = '0';
+    }
   }
 });
 
 window.addEventListener('scroll', () => {
   const scrollPosition: number = window.scrollY;
-  const heroTitle: HTMLElement | null = document.querySelector(
+  const heroTitleRight: HTMLElement | null = document.querySelector(
     '.hero__title__right'
   ) as HTMLElement;
 
+  if (heroTitleRight) {
+    if (window.innerWidth > 765) {
+      const moveDistance: number = 0.7; // Adjust this value to change the distance
+      heroTitleRight.style.marginRight = `-${scrollPosition * moveDistance}px`;
+    } else {
+      heroTitleRight.style.marginRight = '0';
+    }
+  }
+});
+
+window.addEventListener('scroll', () => {
+  const scrollPosition: number = window.scrollY;
+  const heroTitle: HTMLElement | null = document.querySelector('.hero__title');
+
   if (heroTitle) {
+    // Calculate the amount by which to move the title upwards
     const moveDistance: number = 1.5; // Adjust this value to change the distance
-    heroTitle.style.marginRight = `-${scrollPosition * moveDistance}px`;
+    const marginTop: number = scrollPosition * moveDistance;
+    heroTitle.style.marginTop = `-${marginTop}px`;
   }
 });
