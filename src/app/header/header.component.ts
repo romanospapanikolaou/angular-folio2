c5c1f9d620b6ab5a14ac8d79b30786205a480e6e
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-04-27 01:14:25 +0200

    burger

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,8 +5,46 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent {}
 
-}
+(window as any).toggleMenu = () => {
+  const navLinks: HTMLElement | null = document.querySelector('.nav-links');
+  const burger: HTMLElement | null = document.querySelector('.burger');
+  const dropdown: HTMLElement | null =
+    document.querySelector('.dropdown-content');
+
+  if (!navLinks || !burger || !dropdown) {
+    console.error('One or more elements not found.');
+    return;
+  }
+
+  navLinks.classList.toggle('active');
+  burger.classList.toggle('active');
+  dropdown.classList.toggle('active');
+
+  // Close dropdown if screen is larger than 900px and burger is clicked
+  if (window.innerWidth > 900 && !burger.classList.contains('active')) {
+    dropdown.classList.remove('active');
+    burger.classList.remove('active');
+  }
+};
+
+window.onresize = function (): void {
+  const screenWidth: number = window.innerWidth;
+  const burger: HTMLElement | null = document.querySelector('.burger');
+  const dropdown: HTMLElement | null =
+    document.querySelector('.dropdown-content');
+
+  if (!burger || !dropdown) {
+    console.error('One or more elements not found.');
+    return;
+  }
+
+  // Close dropdown and reset burger state if screen is larger than 900px
+  if (screenWidth > 900) {
+    dropdown.classList.remove('active');
+    burger.classList.remove('active');
+  }
+};
