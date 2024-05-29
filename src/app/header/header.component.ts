57928e4626ea538bfc5f365cdb12f59e08f2111e
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-05-29 21:46:02 +0200

    slower smooth scroll

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -17,11 +17,31 @@ export class HeaderComponent {
         .substring(1);
       const targetElement = document.getElementById(targetId);
       if (targetElement) {
-        window.scrollTo({
-          top: targetElement.offsetTop,
-          behavior: 'smooth',
-        });
+        this.smoothScroll(targetElement);
       }
     }
+  }
+
+  smoothScroll(targetElement: HTMLElement) {
+    const duration = 1500; // duration in milliseconds
+    const start = window.pageYOffset;
+    const targetPosition = targetElement.getBoundingClientRect().top;
+    const startTime =
+      'now' in window.performance ? performance.now() : new Date().getTime();
+
+    const scrollStep = () => {
+      const now =
+        'now' in window.performance ? performance.now() : new Date().getTime();
+      const time = Math.min(1, (now - startTime) / duration);
+      const timeFunction = time * (2 - time); // easeOutQuad easing function
+
+      window.scrollTo(0, Math.ceil(timeFunction * targetPosition + start));
+
+      if (time < 1) {
+        requestAnimationFrame(scrollStep);
+      }
+    };
+
+    requestAnimationFrame(scrollStep);
   }
 }
