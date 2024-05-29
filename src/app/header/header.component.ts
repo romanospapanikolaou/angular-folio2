7afe0cb6b299356c85430be3d330d12707ff3a93
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-05-29 16:15:09 +0200

    smooth scolling added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  @HostListener('click', ['$event'])
+  onClick(event: Event) {
+    if ((event.target as HTMLElement).tagName === 'A') {
+      event.preventDefault();
+      const targetId = (event.target as HTMLElement)
+        .getAttribute('href')!
+        .substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth',
+        });
+      }
+    }
+  }
 }
